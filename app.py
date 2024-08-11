7c5489f35acd0aeb2ff62bab231836f70ff812af
angeloxx <angeloxx@angeloxx.it>
2024-08-11 15:14:06 +0200

    yo

--- app.py
+++ app.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+def test_hello_world():
+    assert hello_world() == 'Hello World!'
+
 if __name__ == '__main__':
     app.run()
